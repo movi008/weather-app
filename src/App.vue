ddb8bee2a9e058cd58df2051dd27f2ec79e2a17a
movi008 <socialmedia.ovi@gmail.com>
2023-06-20 21:19:16 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+    
+<div>
+    
+      <HelloWorld msg="You did it!" />
+   
+</div>
+
+  
+</template>
